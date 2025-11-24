835e81f87b61a6737423c10d936d7f9c24fe039c
Julius Foitzik <info@accountr.eu>
2025-11-24 16:31:10 +0100

    fix args

--- src/egress.rs
+++ src/egress.rs
@@ -4,20 +4,6 @@ use std::{
     path::Path,
 };
 
-pub(crate) fn default_csv_egress(filename: &Path) -> std::io::Result<csv::Writer<BufWriter<File>>> {
-    let f = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open(filename)?;
-    // NOTE(juf): The buffer size can/should be adjusted based on the use-case.
-    // NOTE(juf): csv already comes with buffered writes. This is just an example how buffered
-    // output could look like if csv would not have it builtin
-    let buf = BufWriter::new(f);
-    let writer = csv::WriterBuilder::new().from_writer(buf);
-    Ok(writer)
-}
-
 pub(crate) fn stdout_csv_egress() -> std::io::Result<csv::Writer<BufWriter<Stdout>>> {
     // NOTE(juf): The buffer size can/should be adjusted based on the use-case.
     // NOTE(juf): csv already comes with buffered writes. This is just an example how buffered
