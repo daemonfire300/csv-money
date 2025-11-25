fea3a45d677740c8f9a4908112ea6c0ad08dedb0
Julius Foitzik <info@accountr.eu>
2025-11-25 08:50:45 +0100

    minor

--- src/processor.rs
+++ src/processor.rs
@@ -166,7 +166,9 @@ impl Processor {
         };
     }
 
+    #[inline(always)]
     fn create_account_if_not_exists(&mut self, id: u16) -> &Account {
+        // debattable
         self.account_store.entry(id).or_insert(Account::new(id))
     }
 }
